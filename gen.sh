d76d934754c216de43605dea9a11f5b6f188ad37
Marek Majkowski <majek04@gmail.com>
2011-08-03 11:02:23 +0100

    Print time of generation

--- gen.sh
+++ gen.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+T0=`date +%s%0N`
 echo " [*] Generating javascript"
 coffee bin/render.coffee lib/all.js > sockjs.js && \
 coffee bin/render.coffee --minify lib/all.js > sockjs.min.js && \
 coffee bin/render.coffee --minify --pretty lib/all.js > sockjs.pretty.js && \
-echo " [*] ok"
+T1=`date +%s%0N`
+TDMS=$(( ($T1-$T0) / 1000000))
+echo " [*] ok $TDMS ms"
