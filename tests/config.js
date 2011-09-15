bef442b8ca4c4c4dba5d08950c0b917b4b7cd7f7
Marek Majkowski <majek04@gmail.com>
2011-09-15 12:55:32 +0100

    Default config should be generic, not necessarily simple.

--- tests/config.js
+++ tests/config.js
@@ -2,15 +2,15 @@ exports.config = {
     opts: {
         sockjs_url: "/lib/sockjs.js"
     },
-    port: 8080,
+    port: process.env.PORT || 8080,
     host: '0.0.0.0',
 
     client_opts: {
         // May be set to empty string if you don't need to test
         // cross-domain features. In other case set it to a full
         // url, like: "http://localhost:8080"
-        url: 'http://localhost:8080',
         disabled_transports: [],
+        url: '',
         sockjs_opts: {devel:true}
     }
 };
