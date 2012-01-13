832ced3b0f99fc4dc8065634d29262d099366a26
Marek Majkowski <majek04@gmail.com>
2012-01-13 15:39:03 +0000

    Use new detectProtocols method to detect protocols.

--- tests/config.js
+++ tests/config.js
@@ -3,7 +3,11 @@ exports.config = {
         // Address of a sockjs test server.
         url: 'http://localhost:8081',
         disabled_transports: [],
-        sockjs_opts: {devel:true, debug:true}
+        sockjs_opts: {
+            devel:true,
+            debug:true,
+            info: {websocket:true, cookie_need:false}
+        }
     },
 
     port: 8080,
