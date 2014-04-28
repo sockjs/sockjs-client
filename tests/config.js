a5cf05b2a923092c84537941b7a345c6ff1997ea
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-04-28 12:06:55 +0200

    Replaced the deprecated whitelist protocols with WebSocket protocols.
    This commit removes the handling of the deprecated second constructor
    argument replacing it with a protocol parameter. The reason for not
    moving this from the previous options object is to mimic the WebSocket
    API where this is the second parameter.

--- tests/config.js
+++ tests/config.js
@@ -2,11 +2,11 @@ exports.config = {
     client_opts: {
         // Address of a sockjs test server.
         url: 'http://localhost:8081',
+        //protocols: ['sockjs-test', 'bogus'],
         sockjs_opts: {
             devel: true,
             debug: true,
             // websocket:false
-            //subprotocols: ['testing']
             info: {cookie_needed:false},
         }
     },
