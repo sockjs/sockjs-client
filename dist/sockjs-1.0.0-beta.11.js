70c186c4ea6549d458db808ab5fe94778ae407ba
Bryce Kahle <bkahle@gmail.com>
2015-02-01 11:40:26 -0500

    [dist] 1.0.0-beta.11

--- dist/sockjs-1.0.0-beta.11.js
+++ dist/sockjs-1.0.0-beta.11.js
@@ -1,4 +1,4 @@
-/* sockjs-client v1.0.0-beta.10 | http://sockjs.org | MIT license */
+/* sockjs-client v1.0.0-beta.11 | http://sockjs.org | MIT license */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.SockJS=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 'use strict';
@@ -3632,7 +3632,7 @@ module.exports = {
 
 }).call(this,{ env: {} })
 },{"debug":53,"url-parse":58}],52:[function(require,module,exports){
-module.exports = '1.0.0-beta.10';
+module.exports = '1.0.0-beta.11';
 },{}],53:[function(require,module,exports){
 
 /**
