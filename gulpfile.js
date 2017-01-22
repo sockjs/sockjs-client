336cf329c20e71c676d6f9bef29903a1f9b969c1
Bryce Kahle <bkahle@gmail.com>
2017-01-22 14:17:24 -0500

    Simplify build process to non-versioned filenames

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,13 @@ var gulp = require('gulp')
   , envify = require('envify/custom')
   , rename = require('gulp-rename')
   , header = require('gulp-header')
+  , replace = require('gulp-replace')
   , pkg = require('./package.json')
   , fs = require('fs')
   , pump = require('pump')
   ;
 
-var libName = 'sockjs-' + pkg.version
-  , browserifyOptions = {
+var browserifyOptions = {
       entries: './lib/entry.js'
     , standalone: 'SockJS'
     , debug: true
@@ -35,11 +35,12 @@ gulp.task('write-version', function () {
 });
 
 gulp.task('testbundle', ['browserify:min'], function() {
-  gulp.src('./build/' + libName + '.min.js')
+  gulp.src('./build/sockjs.min.js')
     .pipe(rename('sockjs.js'))
+    .pipe(replace('sourceMappingURL=sockjs.min.js.map', 'sourceMappingURL=sockjs.js.map'))
     .pipe(gulp.dest('./tests/html/lib/'));
 
-  return gulp.src('./build/' + libName + '.min.js.map')
+  return gulp.src('./build/sockjs.min.js.map')
     .pipe(rename('sockjs.js.map'))
     .pipe(gulp.dest('./tests/html/lib/'));
 });
@@ -69,7 +70,7 @@ gulp.task('browserify', ['write-version'], function (cb) {
 gulp.task('browserify:min', ['write-version'], function (cb) {
   pump([
     browserify(browserifyOptions).exclude('debug').transform(envify({ NODE_ENV: 'production' })).bundle(),
-    source(libName + '.min.js'),
+    source('sockjs.min.js'),
     buffer(),
     sourcemaps.init({ loadMaps: true }),
     uglify({ compress: { pure_funcs: ['debug'] } }),
@@ -80,37 +81,12 @@ gulp.task('browserify:min', ['write-version'], function (cb) {
 });
 
 gulp.task('release', ['browserify', 'browserify:min'], function () {
-  // sockjs-{version}.min.js
-  gulp.src('./build/' + libName + '.min.js')
-    .pipe(gulp.dest('./dist/'));
-
-  gulp.src('./build/' + libName + '.min.js.map')
-    .pipe(gulp.dest('./dist/'));
-
-  // sockjs-{version}.js
   gulp.src('./build/sockjs.js')
-    .pipe(rename(libName + '.js'))
-    .pipe(gulp.dest('./dist/'));
-
-  gulp.src('./build/sockjs.js.map')
-    .pipe(rename(libName + '.js.map'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('stable-release', ['release'], function () {
-  // sockjs.min.js + sockjs.min.js.map
-  gulp.src('./build/' + libName + '.min.js')
-    .pipe(rename('sockjs.min.js'))
-    .pipe(gulp.dest('./dist/'));
-  gulp.src('./build/' + libName + '.min.js.map')
-    .pipe(rename('sockjs.min.js.map'))
-    .pipe(gulp.dest('./dist/'));
-
-  // sockjs.js + sockjs.js.map
-  gulp.src('./build/sockjs.js')
-    .pipe(rename('sockjs.js'))
     .pipe(gulp.dest('./dist/'));
   gulp.src('./build/sockjs.js.map')
-    .pipe(rename('sockjs.js.map'))
+    .pipe(gulp.dest('./dist/'));
+  gulp.src('./build/sockjs.min.js')
+    .pipe(gulp.dest('./dist/'));
+  gulp.src('./build/sockjs.min.js.map')
     .pipe(gulp.dest('./dist/'));
 });
