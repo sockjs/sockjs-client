81c5e12419165d5a65f6194800726523c7940a07
Pertsev Roman <pertsev.roman@gmail.com>
2017-06-08 11:19:48 +0300

    declare class

--- dist/sockjs.d.ts
+++ dist/sockjs.d.ts
@@ -2,6 +2,13 @@
  * Created by RAPertsev on 08.06.2017.
  */
 
-export declare interface SockJS {
+export declare class SockJS {
     new(url: string): SockJS;
+
+    onopen: Function;
+    onclose: Function;
+    onmessage: Function;
+
+    send(message: string): void;
+    close(): void;
 }
