2db49fc6fc785c19a6f7290d366205c8f2b3aae4
Pertsev Roman <pertsev.roman@gmail.com>
2017-06-08 11:05:10 +0300

    экспорт функции

--- dist/sockjs.d.ts
+++ dist/sockjs.d.ts
@@ -2,13 +2,4 @@
  * Created by RAPertsev on 08.06.2017.
  */
 
-export declare class SockJS {
-    onopen: Function;
-    onmessage: Function;
-    onclose: Function;
-
-    constructor(url: string);
-
-    send(message: string): void;
-    close(): void;
-}
+export declare function SockJS(url: string);
