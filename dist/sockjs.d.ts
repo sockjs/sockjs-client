7e18123227ee2462f23aaea4c8293aa101ec9e56
Pertsev Roman <pertsev.roman@gmail.com>
2017-06-08 11:23:00 +0300

    declare functions

--- dist/sockjs.d.ts
+++ dist/sockjs.d.ts
@@ -2,13 +2,4 @@
  * Created by RAPertsev on 08.06.2017.
  */
 
-export declare class SockJS {
-    new(url: string): SockJS;
-
-    onopen: Function;
-    onclose: Function;
-    onmessage: Function;
-
-    send(message: string): void;
-    close(): void;
-}
+export declare function SockJS(url: string, proto: any, options: any);
