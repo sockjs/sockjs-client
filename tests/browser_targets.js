6c9c6b6cb28fe6582bf675e4f2868759f7a08aa2
Bryce Kahle <bkahle@gmail.com>
2018-09-20 15:38:33 -0400

    Change browser targest to latest of everything

--- tests/browser_targets.js
+++ tests/browser_targets.js
@@ -20,27 +20,27 @@ module.exports = {
     os: 'Windows',
     os_version: '10'
   },
-  bs_ie_10_windows: {
-    base: 'BrowserStack',
-    browser: 'ie',
-    browser_version: '10.0',
-    os: 'Windows',
-    os_version: '8'
-  },
-  bs_ie_9_windows: {
-    base: 'BrowserStack',
-    browser: 'ie',
-    browser_version: '9.0',
-    os: 'Windows',
-    os_version: '7'
-  },
-  bs_ie_8_windows: {
-    base: 'BrowserStack',
-    browser: 'ie',
-    browser_version: '8.0',
-    os: 'Windows',
-    os_version: '7'
-  },
+  // bs_ie_10_windows: {
+  //   base: 'BrowserStack',
+  //   browser: 'ie',
+  //   browser_version: '10.0',
+  //   os: 'Windows',
+  //   os_version: '8'
+  // },
+  // bs_ie_9_windows: {
+  //   base: 'BrowserStack',
+  //   browser: 'ie',
+  //   browser_version: '9.0',
+  //   os: 'Windows',
+  //   os_version: '7'
+  // },
+  // bs_ie_8_windows: {
+  //   base: 'BrowserStack',
+  //   browser: 'ie',
+  //   browser_version: '8.0',
+  //   os: 'Windows',
+  //   os_version: '7'
+  // },
   bs_firefox_mac: {
     base: 'BrowserStack',
     browser: 'firefox',
@@ -55,41 +55,41 @@ module.exports = {
     os: 'OS X',
     os_version: 'High Sierra'
   },
-  bs_safari_10_mac: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '10.1',
-    os: 'OS X',
-    os_version: 'Sierra'
-  },
-  bs_safari_9_mac: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '9.1',
-    os: 'OS X',
-    os_version: 'El Capitan'
-  },
-  bs_safari_8_mac: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '8.0',
-    os: 'OS X',
-    os_version: 'Yosemite'
-  },
-  bs_safari_7_mac: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '7.0',
-    os: 'OS X',
-    os_version: 'Mavericks'
-  },
-  bs_safari_6_mac: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '6.0',
-    os: 'OS X',
-    os_version: 'Mountain Lion'
-  },
+  // bs_safari_10_mac: {
+  //   base: 'BrowserStack',
+  //   browser: 'safari',
+  //   browser_version: '10.1',
+  //   os: 'OS X',
+  //   os_version: 'Sierra'
+  // },
+  // bs_safari_9_mac: {
+  //   base: 'BrowserStack',
+  //   browser: 'safari',
+  //   browser_version: '9.1',
+  //   os: 'OS X',
+  //   os_version: 'El Capitan'
+  // },
+  // bs_safari_8_mac: {
+  //   base: 'BrowserStack',
+  //   browser: 'safari',
+  //   browser_version: '8.0',
+  //   os: 'OS X',
+  //   os_version: 'Yosemite'
+  // },
+  // bs_safari_7_mac: {
+  //   base: 'BrowserStack',
+  //   browser: 'safari',
+  //   browser_version: '7.0',
+  //   os: 'OS X',
+  //   os_version: 'Mavericks'
+  // },
+  // bs_safari_6_mac: {
+  //   base: 'BrowserStack',
+  //   browser: 'safari',
+  //   browser_version: '6.0',
+  //   os: 'OS X',
+  //   os_version: 'Mountain Lion'
+  // },
   bs_ios_11: {
     base: 'BrowserStack',
     browser: 'iphone',
@@ -99,15 +99,15 @@ module.exports = {
     device: 'iPhone 6S',
     real_mobile: true
   },
-  bs_ios_10: {
-    base: 'BrowserStack',
-    browser: 'iphone',
-    browser_version: null,
-    os: 'ios',
-    os_version: '10.3',
-    device: 'iPhone 7',
-    real_mobile: true
-  },
+  // bs_ios_10: {
+  //   base: 'BrowserStack',
+  //   browser: 'iphone',
+  //   browser_version: null,
+  //   os: 'ios',
+  //   os_version: '10.3',
+  //   device: 'iPhone 7',
+  //   real_mobile: true
+  // },
   bs_android_9: {
     base: 'BrowserStack',
     browser: 'android',
@@ -117,31 +117,31 @@ module.exports = {
     device: 'Google Pixel 2',
     real_mobile: true
   },
-  bs_android_8: {
-    base: 'BrowserStack',
-    browser: 'android',
-    browser_version: null,
-    os: 'android',
-    os_version: '8.0',
-    device: 'Samsung Galaxy S9',
-    real_mobile: true
-  },
-  bs_android_7_1: {
-    base: 'BrowserStack',
-    browser: 'android',
-    browser_version: null,
-    os: 'android',
-    os_version: '7.1',
-    device: 'Samsung Galaxy Note 8',
-    real_mobile: true
-  },
-  bs_android_6: {
-    base: 'BrowserStack',
-    browser: 'android',
-    browser_version: null,
-    os: 'android',
-    os_version: '6.0',
-    device: 'Samsung Galaxy S7',
-    real_mobile: true
-  }
+  // bs_android_8: {
+  //   base: 'BrowserStack',
+  //   browser: 'android',
+  //   browser_version: null,
+  //   os: 'android',
+  //   os_version: '8.0',
+  //   device: 'Samsung Galaxy S9',
+  //   real_mobile: true
+  // },
+  // bs_android_7_1: {
+  //   base: 'BrowserStack',
+  //   browser: 'android',
+  //   browser_version: null,
+  //   os: 'android',
+  //   os_version: '7.1',
+  //   device: 'Samsung Galaxy Note 8',
+  //   real_mobile: true
+  // },
+  // bs_android_6: {
+  //   base: 'BrowserStack',
+  //   browser: 'android',
+  //   browser_version: null,
+  //   os: 'android',
+  //   os_version: '6.0',
+  //   device: 'Samsung Galaxy S7',
+  //   real_mobile: true
+  // }
 };
