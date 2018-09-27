ee5f4ca72c1beed11135c1dc2c6f0ee625b59a19
Bryce Kahle <bkahle@gmail.com>
2018-09-27 11:31:46 -0400

    Update test app for sockjs 0.4.0

--- tests/support/sockjs_app.js
+++ tests/support/sockjs_app.js
@@ -4,8 +4,7 @@ var sockjs = require('sockjs');
 var debug = require('debug')('sockjs-client:test-server:app');
 
 exports.install = function(opts, server) {
-  var sjs_echo = sockjs.createServer(opts);
-  sjs_echo.on('connection', function(conn) {
+  var echoHandler = function(conn) {
     debug('    [+] echo open    ' + conn);
     conn.on('close', function() {
       debug('    [-] echo close   ' + conn);
@@ -17,9 +16,21 @@ exports.install = function(opts, server) {
                   ((d.length > 64) ? '...' : ''));
       conn.write(m);
     });
-  });
+  };
+  
+  var sjs_echo = sockjs.createServer(Object.assign({}, opts, {prefix: '/echo', response_limit: 4096}));
+  sjs_echo.on('connection', echoHandler);
+  sjs_echo.attach(server);
 
-  var sjs_close = sockjs.createServer(opts);
+  var sjs_echo2 = sockjs.createServer(Object.assign({}, opts, {prefix: '/disabled_websocket_echo', websocket: false}));
+  sjs_echo2.on('connection', echoHandler);
+  sjs_echo2.attach(server);
+
+  var sjs_echo3 = sockjs.createServer(Object.assign({}, opts, {prefix: '/cookie_needed_echo', jsessionid: true}));
+  sjs_echo3.on('connection', echoHandler);
+  sjs_echo3.attach(server);
+
+  var sjs_close = sockjs.createServer(Object.assign({}, opts, {prefix: '/close'}));
   sjs_close.on('connection', function(conn) {
     debug('    [+] close open    ' + conn);
     conn.close(3000, 'Go away!');
@@ -27,8 +38,9 @@ exports.install = function(opts, server) {
       debug('    [-] close close   ' + conn);
     });
   });
+  sjs_close.attach(server);
 
-  var sjs_ticker = sockjs.createServer(opts);
+  var sjs_ticker = sockjs.createServer(Object.assign({}, opts, {prefix: '/ticker'}));
   sjs_ticker.on('connection', function(conn) {
     debug('    [+] ticker open   ' + conn);
     var tref;
@@ -42,9 +54,10 @@ exports.install = function(opts, server) {
       debug('    [-] ticker close   ' + conn);
     });
   });
+  sjs_ticker.attach(server);
 
   var broadcast = {};
-  var sjs_broadcast = sockjs.createServer(opts);
+  var sjs_broadcast = sockjs.createServer(Object.assign({}, opts, {prefix: '/broadcast'}));
   sjs_broadcast.on('connection', function(conn) {
     debug('    [+] broadcast open ' + conn);
     broadcast[conn.id] = conn;
@@ -54,13 +67,14 @@ exports.install = function(opts, server) {
     });
     conn.on('data', function(m) {
       debug('    [-] broadcast message', m);
-      for(var id in broadcast) {
-          broadcast[id].write(m);
+      for (var id in broadcast) {
+        broadcast[id].write(m);
       }
     });
   });
+  sjs_broadcast.attach(server);
 
-  var sjs_amplify = sockjs.createServer(opts);
+  var sjs_amplify = sockjs.createServer(Object.assign({}, opts, {prefix: '/amplify'}));
   sjs_amplify.on('connection', function(conn) {
     debug('    [+] amp open    ' + conn);
     conn.on('close', function() {
@@ -73,16 +87,5 @@ exports.install = function(opts, server) {
       conn.write(new Array(Math.pow(2, n) + 1).join('x'));
     });
   });
-
-
-  sjs_echo.installHandlers(server, {prefix: '/echo',
-                                    response_limit: 4096});
-  sjs_echo.installHandlers(server, {prefix: '/disabled_websocket_echo',
-                                    websocket: false});
-  sjs_echo.installHandlers(server, {prefix: '/cookie_needed_echo',
-                                    jsessionid: true});
-  sjs_close.installHandlers(server, {prefix: '/close'});
-  sjs_ticker.installHandlers(server, {prefix: '/ticker'});
-  sjs_amplify.installHandlers(server, {prefix: '/amplify'});
-  sjs_broadcast.installHandlers(server, {prefix: '/broadcast'});
+  sjs_amplify.attach(server);
 };
