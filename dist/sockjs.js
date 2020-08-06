c135fb4d8aa731901ca28975b1206622a377c0c0
Pawel Czekaj <pawel.czekaj@ziflow.com>
2020-08-06 12:05:48 +0200

    bugfix: fixed incorrect name

--- dist/sockjs.js
+++ dist/sockjs.js
@@ -1,4 +1,4 @@
-/* sockjs-client v1.5.0 | http://sockjs.org | MIT license */
+/* sockjs-client v1.5.1-pre | http://sockjs.org | MIT license */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SockJS = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 'use strict';
@@ -2243,7 +2243,7 @@ if (process.env.NODE_ENV !== 'production') {
   debug = require('debug')('sockjs-client:receiver:eventsource');
 }
 
-function decodeURSafeI(s) {
+function decodeURISafe(s) {
   return decodeURI(s.replace(/%(?![0-9][0-9a-fA-F]+)/g, '%25'));
 }
 
@@ -3740,7 +3740,7 @@ module.exports = {
 }).call(this,{ env: {} })
 
 },{"debug":55,"url-parse":61}],53:[function(require,module,exports){
-module.exports = '1.5.0';
+module.exports = '1.5.1-pre';
 
 },{}],54:[function(require,module,exports){
 /**
