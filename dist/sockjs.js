a2d715dc9aa2f35e951441abcb439b82f3b6d0ec
Pawel Czekaj <pawel.czekaj@ziflow.com>
2022-11-08 08:50:49 +0100

    feat:  clear timeout after connection close

--- dist/sockjs.js
+++ dist/sockjs.js
@@ -1,4 +1,4 @@
-/* sockjs-client v1.6.0 | http://sockjs.org | MIT license */
+/* sockjs-client v1.6.1 | http://sockjs.org | MIT license */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SockJS = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){(function (){
 'use strict';
@@ -904,6 +904,8 @@ SockJS.prototype._transportClose = function(code, reason) {
     this._transport.removeAllListeners();
     this._transport = null;
     this.transport = null;
+    this._transportTimeoutId = null;
+    clearTimeout(this._transportTimeoutId);
   }
 
   if (!userSetCode(code) && code !== 2000 && this.readyState === SockJS.CONNECTING) {
@@ -3730,7 +3732,7 @@ module.exports = {
 }).call(this)}).call(this,{ env: {} })
 
 },{"debug":55,"url-parse":60}],53:[function(require,module,exports){
-module.exports = '1.6.0';
+module.exports = '1.6.1';
 
 },{}],54:[function(require,module,exports){
 /**
